e4b1c034c76b3d4dfa6fddaaccced13d4b7b78d0
Moritz Halbritter <mkammerer@vmware.com>
2022-06-13 13:44:51 +0200

    Remove Tomcat hints

--- spring-native-configuration/src/main/java/org/apache/catalina/TomcatHints.java
+++ spring-native-configuration/src/main/java/org/apache/catalina/TomcatHints.java
@@ -17,29 +17,15 @@
 package org.apache.catalina;
 
 import org.apache.catalina.startup.Tomcat;
-import org.apache.coyote.AbstractProtocol;
-import org.apache.coyote.http11.Http11NioProtocol;
 
 import org.springframework.boot.autoconfigure.web.CommonWebInfos;
 import org.springframework.boot.web.embedded.tomcat.TomcatEmbeddedWebappClassLoader;
-import org.springframework.nativex.hint.InitializationHint;
-import org.springframework.nativex.hint.InitializationTime;
-import org.springframework.nativex.hint.MethodHint;
-import org.springframework.nativex.hint.ResourceHint;
-import org.springframework.nativex.type.NativeConfiguration;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.TypeHint;
+import org.springframework.nativex.type.NativeConfiguration;
 
 @NativeHint(trigger = Tomcat.class, types = {
-		@TypeHint(types = TomcatEmbeddedWebappClassLoader.class),
-		@TypeHint(types = AbstractProtocol.class, methods = @MethodHint(name = "getLocalPort"), access = {}),
-		@TypeHint(types = Http11NioProtocol.class),
-}, initialization = {
-		@InitializationHint(types = {
-				org.apache.catalina.servlets.DefaultServlet.class,
-				org.apache.catalina.Globals.class
-		}, initTime = InitializationTime.BUILD)
-}, resources = @ResourceHint(patterns = "org/apache/catalina/startup/MimeTypeMappings.properties")
-, imports = CommonWebInfos.class)
+		@TypeHint(types = TomcatEmbeddedWebappClassLoader.class)
+}, imports = CommonWebInfos.class)
 public class TomcatHints implements NativeConfiguration {
 }
